58c8d9f7c8c42ef718ca003f74c86bb94d04c5e7
eduancuk <eduardusoldi@outlook.com>
2025-03-24 15:38:09 +0700

    feat: adding app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
