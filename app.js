41ae2b615933bde158b7415fb2ef3d26a5bc0b05
eduancuk <eduardusoldi@outlook.com>
2025-03-24 16:21:35 +0700

    fix: connecting to dockerfile

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || "mongodb://mongodb:27017/mydatabase";
 
-app.use(cors());
-app.use(express.json());
+
+app.use(cors()); 
+app.use(express.json()); 
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error(err));
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB inside Docker!"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
+// Sample route
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("Hello World!");
 });
 
+// Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
